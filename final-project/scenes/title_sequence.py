0c8dca610b5c4d247415595a970b1ec9849260a4
katyareichert <katerinamreichert@gmail.com>
2023-04-24 19:46:12 +0100

    Completed animations for title sequence

--- final-project/scenes/title_sequence.py
+++ final-project/scenes/title_sequence.py
@@ -9,26 +9,37 @@ pygame.mixer.init()
 # Define screen constants
 WIDTH, HEIGHT = 150*5, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-# BG = pygame.transform.scale(pygame.image.load(os.path.join('../assets/minigame', 'tumblr.png')), (WIDTH, HEIGHT))
 
-# Define movement constants
+# Read in background images
+TITLE_TEXT = pygame.transform.scale(pygame.image.load(os.path.join('../assets/title_seq', 'title2.png')), 
+                                    (WIDTH, HEIGHT))
+START_GIF = [pygame.transform.scale(pygame.image.load(os.path.join('../assets/title_seq', 'frame_' + 
+                                        str(i) + '.png')), (WIDTH, HEIGHT)) for i in range(0,12)]
+MAIN_GIF = [pygame.transform.scale(pygame.image.load(os.path.join('../assets/title_seq', 'main_' + 
+                                        str(i) + '.png')), (WIDTH, HEIGHT)) for i in range(0,3)]
+
+# Define movement constantss
 FPS = 60
 
 pygame.display.set_caption("Comfort Cafe")
 
-def draw_window(bg_counter, text_opacity):
-    
-    # draw background
-    # WIN.blit(BG, (0, 0))
-    
+def draw_window():
     # update
     pygame.display.update()
 
-def background_animation(bg_counter):
-    if bg_counter < 20:
-        WIN.blit()
-    bg_counter += 1
+def starting_animation(bg_counter):
+    WIN.blit(START_GIF[bg_counter], (0,0))
 
+def main_animation(i):
+    WIN.blit(MAIN_GIF[i], (0,0))
+
+def fade_in_text(text_opacity):
+    if text_opacity < 255:
+        trans_text = TITLE_TEXT.copy()
+        trans_text.fill((255, 255, 255, text_opacity), None, pygame.BLEND_RGBA_MULT)
+        WIN.blit(trans_text, (0,0))
+    else:
+        WIN.blit(TITLE_TEXT, (0,0))
 
 def main():
     # initialize clock
@@ -37,7 +48,7 @@ def main():
 
     elapsed_time_ctr = 0
     bg_counter = 0
-    text_opacity = 0
+    text_opacity = 5
 
     # game loop
     while(run):
@@ -46,13 +57,19 @@ def main():
         t = clock.get_time()
         elapsed_time_ctr += t
         
-        if elapsed_time_ctr >= 16.5*15:
-            background_animation(bg_counter)
+        if elapsed_time_ctr >= 247.5:
+            if bg_counter < 12:
+                starting_animation(bg_counter)
+            else:
+                main_animation(bg_counter%3)
 
-            if bg_counter < 20:
-                bg_counter += 1
-
+            bg_counter += 1
             elapsed_time_ctr = 0
+
+        if bg_counter > 12:
+            fade_in_text(text_opacity)
+            if text_opacity < 255:
+                text_opacity += 10
 
 
         for event in pygame.event.get():
