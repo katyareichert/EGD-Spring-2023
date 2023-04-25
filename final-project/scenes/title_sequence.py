d91a5ebf7d1e37496a241b400795574ea0bbba8f
katyareichert <katerinamreichert@gmail.com>
2023-04-25 13:24:40 +0100

    Added selection square

--- final-project/scenes/title_sequence.py
+++ final-project/scenes/title_sequence.py
@@ -11,15 +11,18 @@ WIDTH, HEIGHT = 150*5, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Read in background images
-TITLE_TEXT = pygame.transform.scale(pygame.image.load(os.path.join('../assets/title_seq', 'title2.png')), 
+TITLE_TEXT = pygame.transform.scale(pygame.image.load(os.path.join('../assets/title_seq', 'title.png')), 
                                     (WIDTH, HEIGHT))
 START_GIF = [pygame.transform.scale(pygame.image.load(os.path.join('../assets/title_seq', 'frame_' + 
                                         str(i) + '.png')), (WIDTH, HEIGHT)) for i in range(0,12)]
 MAIN_GIF = [pygame.transform.scale(pygame.image.load(os.path.join('../assets/title_seq', 'main_' + 
                                         str(i) + '.png')), (WIDTH, HEIGHT)) for i in range(0,3)]
 
-# Define movement constantss
+# Define movement constants
 FPS = 60
+
+# Define colors
+WHITE = (255,255,255)
 
 pygame.display.set_caption("Comfort Cafe")
 
@@ -33,13 +36,14 @@ def starting_animation(bg_counter):
 def main_animation(i):
     WIN.blit(MAIN_GIF[i], (0,0))
 
-def fade_in_text(text_opacity):
+def fade_in_text(text_opacity, selected_pos=None):
     if text_opacity < 255:
         trans_text = TITLE_TEXT.copy()
         trans_text.fill((255, 255, 255, text_opacity), None, pygame.BLEND_RGBA_MULT)
         WIN.blit(trans_text, (0,0))
     else:
         WIN.blit(TITLE_TEXT, (0,0))
+        pygame.draw.rect(WIN, WHITE, selected_pos)
 
 def main():
     # initialize clock
@@ -49,6 +53,7 @@ def main():
     elapsed_time_ctr = 0
     bg_counter = 0
     text_opacity = 5
+    select_indicator = pygame.Rect(510, 200, 10, 10)
 
     # game loop
     while(run):
@@ -57,24 +62,38 @@ def main():
         t = clock.get_time()
         elapsed_time_ctr += t
         
-        if elapsed_time_ctr >= 247.5:
-            if bg_counter < 12:
+        # Handle background
+        if bg_counter < 12:
                 starting_animation(bg_counter)
-            else:
-                main_animation(bg_counter%3)
-
+        else:
+            main_animation(bg_counter%3)
+            
+        if elapsed_time_ctr >= 247.5:
             bg_counter += 1
             elapsed_time_ctr = 0
 
-        if bg_counter > 12:
-            fade_in_text(text_opacity)
-            if text_opacity < 255:
-                text_opacity += 10
-
-
+        # Handle Menu selection
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+            if text_opacity == 255:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        select_indicator = pygame.Rect(510, 200, 10, 10)
+
+                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        select_indicator = pygame.Rect(510, 270, 10, 10)
+                        
+
+        # Handle title text
+        if bg_counter > 12:
+            if text_opacity < 254:
+                fade_in_text(text_opacity)
+                text_opacity += 10
+            else:
+                fade_in_text(text_opacity, select_indicator)
+
 
         draw_window()
 
