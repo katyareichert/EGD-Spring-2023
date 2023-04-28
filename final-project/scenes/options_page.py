b8d4df84ebaca787b0450d998f1da187b9f6b1a9
katyareichert <katerinamreichert@gmail.com>
2023-04-28 15:55:34 +0100

    Enable selecting options

--- final-project/scenes/options_page.py
+++ final-project/scenes/options_page.py
@@ -21,32 +21,37 @@ class OptionsPage:
         # Define colors
         self.WHITE = (255,255,255)
         self.BROWN = (60,45,31)
+        self.YELLOW = (234,196,146)
 
-        # Define fonts
+        # Define text
         self.BIG_FONT = font1
         self.SMALL_FONT = font2
         
-        self.VOL_TXT = self.BIG_FONT.render("Volume", 1, self.BROWN)
-        self.CHAR_TXT = self.BIG_FONT.render("Character", 1, self.BROWN)
-        self.RETURN_TXT = self.SMALL_FONT.render("Return to Game", 1, self.BROWN)
-        self.EXIT_TXT = self.SMALL_FONT.render("Quit Game", 1, self.BROWN)
+        self.TEXTS = [self.BIG_FONT.render("Volume", 1, self.BROWN),
+                      self.BIG_FONT.render("Character", 1, self.BROWN),
+                      self.SMALL_FONT.render("Return to Game", 1, self.BROWN),
+                      self.SMALL_FONT.render("Quit Game", 1, self.BROWN)
+        ]
+        
+        self.TXT_RECTS = [self.TEXTS[0].get_rect(center=(self.WIDTH//2 + 10, self.HEIGHT//2)),
+                          self.TEXTS[1].get_rect(center=(self.WIDTH//2 + 10, self.HEIGHT//2 + 100)),
+                          self.TEXTS[2].get_rect(center=(self.WIDTH//2 + 10, self.HEIGHT//2 + 190)),
+                          self.TEXTS[3].get_rect(center=(self.WIDTH//2 + 10, self.HEIGHT//2 + 220))
+        ]
 
-        self.VOL_RECT = self.VOL_TXT.get_rect(center=(self.WIDTH//2 + 10, self.HEIGHT//2))
-        self.CHAR_RECT = self.CHAR_TXT.get_rect(center=(self.WIDTH//2 + 10, self.HEIGHT//2 + 100))
-        self.RETURN_RECT = self.RETURN_TXT.get_rect(center=(self.WIDTH//2 + 10, self.HEIGHT//2 + 200))
-        self.EXIT_RECT = self.EXIT_TXT.get_rect(center=(self.WIDTH//2 + 10, self.HEIGHT//2 + 230))
 
-    def draw_window(self, bg_counter):
+    def draw_window(self, bg_counter, selected_i):
 
         # Draw background
         self.WIN.blit(self.CREDIT_PAGE, (0,0))
         self.WIN.blit(self.CREDIT_GIF[bg_counter%8], (0,0))
 
+        # Draw rectangle
+        pygame.draw.rect(self.WIN, self.YELLOW, self.TXT_RECTS[selected_i])
+
         # Draw text
-        self.WIN.blit(self.VOL_TXT, self.VOL_RECT)
-        self.WIN.blit(self.CHAR_TXT, self.CHAR_RECT)
-        self.WIN.blit(self.RETURN_TXT, self.RETURN_RECT)
-        self.WIN.blit(self.EXIT_TXT, self.EXIT_RECT)
+        for i in range(len(self.TEXTS)):
+            self.WIN.blit(self.TEXTS[i], self.TXT_RECTS[i])
 
         # update
         pygame.display.update()
@@ -58,6 +63,7 @@ class OptionsPage:
 
         elapsed_time_ctr = 0
         bg_counter = 0
+        selected_i = 0
 
         # game loop
         while(run):
@@ -75,26 +81,36 @@ class OptionsPage:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP or event.key == pygame.K_w:
                         # move option
-                        pass
-
+                        if selected_i > 0:
+                            selected_i -= 1
+                        
                     if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                         # move option
-                        pass
+                        if selected_i < len(self.TEXTS)-1:
+                            selected_i += 1
 
                     if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                         # change volume or select character
-                        self.VOLUME = round(self.VOLUME - 0.2, 1)
-                        pygame.mixer.music.set_volume(self.VOLUME)
+                        if selected_i == 0:
+                            self.VOLUME = round(self.VOLUME - 0.2, 1)
+                            pygame.mixer.music.set_volume(self.VOLUME)
 
                     if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                         # change volume or select character
-                        self.VOLUME = round(self.VOLUME + 0.2, 1)
-                        pygame.mixer.music.set_volume(self.VOLUME)
+                        if selected_i == 0:
+                            self.VOLUME = round(self.VOLUME + 0.2, 1)
+                            pygame.mixer.music.set_volume(self.VOLUME)
 
                     if event.key == pygame.K_RETURN:
-                        return
+                        if selected_i < len(self.TEXTS)-2:
+                            selected_i += 1
+                        elif selected_i == len(self.TEXTS)-2:
+                            return
+                        else:
+                            pygame.quit()
+                            exit()
   
-            self.draw_window(bg_counter)
+            self.draw_window(bg_counter, selected_i)
 
             if elapsed_time_ctr >= 375:
                 bg_counter += 1
